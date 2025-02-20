b1c159f4723aebb667228245f7deb77a052b107c
Simon Shakeshaft <simon.shak3shaft@gmail.com>
2025-02-20 16:27:18 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 
 cnx=st.connection("snowflake")
 session = cnx.session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop
 
