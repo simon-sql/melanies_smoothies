35ded1a5966ec5249b4ec37b388eeef2cb297597
Simon Shakeshaft <simon.shak3shaft@gmail.com>
2025-02-20 15:56:52 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
 
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     #st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
